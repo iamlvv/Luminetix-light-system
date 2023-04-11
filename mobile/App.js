f4ede951b044770e4821ffaec3e6de81e65fdcb0
iamlvv <levanvy28062002@gmail.com>
2023-04-11 16:46:32 +0700

    init navigation bottom bar

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Homepage from './src/screens/Homepage/Homepage';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Statistics from './src/screens/Statistics/Statistics';
+import ManualControl from './src/screens/ManualControl/ManualControl';
+import ContextSetup from './src/screens/ContextSetup/ContextSetup';
+import UserProfile from './src/screens/UserProfile/UserProfile';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons'; 
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Homepage}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+            tabBarActiveTintColor: 'purple',
+            tabBarInactiveTintColor: 'gray',
+            headerShown: false,
+          }}
+
+        />
+        <Tab.Screen name="Statistics" component={Statistics} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="stats-chart" color={color} size={size} />
+            ),
+            tabBarActiveTintColor: 'purple',
+            tabBarInactiveTintColor: 'gray',
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen name='Manual Control' component={ManualControl} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="remote" color={color} size={size} />
+            ),
+            tabBarActiveTintColor: 'purple',
+            tabBarInactiveTintColor: 'gray',
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen name='Context Setup' component={ContextSetup} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="settings" color={color} size={size} />
+            ),
+            tabBarActiveTintColor: 'purple',
+            tabBarInactiveTintColor: 'gray',
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen name='User Profile' component={UserProfile} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="user" color={color} size={size} />
+            ),
+            tabBarActiveTintColor: 'purple',
+            tabBarInactiveTintColor: 'gray',
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- mobile/src/components/Navigator.jsx
+++ mobile/src/components/Navigator.jsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Statistics = () => {
+const Navigator = () => {
   return (
     <View>
-      <Text>Statistics</Text>
+      <Text>
+      </Text>
     </View>
   )
 }
 
-export default Statistics
+export default Navigator
