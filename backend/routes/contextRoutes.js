f52370b09c7d5fb0b7660970214757ca3cb4b018
ppmblue <nhu1142002@gmail.com>
2023-04-23 03:16:36 +0700

    Protect context API

--- backend/routes/contextRoutes.js
+++ backend/routes/contextRoutes.js
@@ -13,7 +13,7 @@ const { protect } = require("../middleware/authMiddleware.js");
 router
 .route("/")
 .get(protect, getAllContexts)
-.post(createContext);
+.post(protect,createContext);
 router
 .route("/:id")
 .delete(protect,deleteContext)
