9c68ae45fbe9686e11368aba1cc4509fe311f600
ppmblue <nhu1142002@gmail.com>
2023-04-26 08:22:07 +0700

    delete all api & fix context control

--- backend/routes/contextRoutes.js
+++ backend/routes/contextRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {
   createContext,
   deleteContext,
+  deleteAllContext,
   getAllContexts,
   toggleContext,
   updateContext
@@ -13,7 +14,8 @@ const { protect } = require("../middleware/authMiddleware.js");
 router
 .route("/")
 .get(protect, getAllContexts)
-.post(protect,createContext);
+.post(protect,createContext)
+.delete(protect,deleteAllContext);
 router
 .route("/:id")
 .delete(protect,deleteContext)
